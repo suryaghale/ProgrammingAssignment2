f8d21077b7b7dd6b497afe578496174c715e4574
suryaghale <nepalghale@gmail.com>
2017-02-17 17:21:48 +0800

    Assignment 2 by Surya Sing Gurung Ghale
    modified cachematrix.R which includes  functions for caching inverse of
    a matrix to quicker retrieval

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Author : SSGG
+# Assignment 2
+# Function Name : makeCacheMatrix
+# Functionality : Cache the Inverse of a Matrix
+# This function creates a matrix object and caches its own inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(mt = matrix()) 
+  {
+    inv <- NULL
+    set <- function(y) 
+      {
+      mt <<- y
+      inv <<- NULL
+      }
+  get <- function() mt
+  setInverse <- function(inverse) 
+  inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+# Function Name : cacheSolve
+# Functionality : Compute the inverse of the matrix
+# If the inverse has already been calculated it should retrieves the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(mt, ...) 
+  {
+  inv <- mt$getInverse()
+  if (!is.null(inv)) 
+    {
+    inv
+    }
+  else
+    {
+    mtx <- mt$get()
+    inv <- solve(mtx, ...)
+    mt$setInverse(inv)
+    inv
+    }
+  }
+
+#Example
+#a <- diag(2,4)
+#cmtx <- makeCacheMatrix(a)
+#ia <- cacheSolve(cmtx)
